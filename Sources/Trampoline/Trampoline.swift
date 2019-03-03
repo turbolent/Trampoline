d9750f9e7202ed686b9588f309baadd3d10cf7dd
Bastian Müller <bastian@turbolent.com>
2019-03-03 12:44:15 -0800

    simplify

--- Sources/Trampoline/Trampoline.swift
+++ Sources/Trampoline/Trampoline.swift
@@ -2,24 +2,24 @@
 // Based on "Stackless Scala With Free" by Rúnar Óli Bjarnason:
 // http://blog.higher-order.com/assets/trampolines.pdf
 
-/**
- Trampolines allow computations to be executed in constant stack space,
- by trading it for heap space. They can be used for computations which
- would otherwise use a large amount of stack space, potentially crashing
- when the limited amount is exhausted (stack overflow).
-
- A Trampoline represents a computation which consists of steps.
- Each step is either more work which should be executed (`More`),
- in the form of a function which returns the next step,
- or a final value (`Done`), which indicates the end of the computation.
-
- In trampolined programs, instead of each computation invoking
- the next computation, (i.e., calling functions, possibly recursing directly),
- they yield the next computation.
-
- Trampolines can be executed through a control loop using the `run` method,
- and can be chained together using the `flatMap` method.
-*/
+///
+/// Trampolines allow computations to be executed in constant stack space,
+/// by trading it for heap space. They can be used for computations which
+/// would otherwise use a large amount of stack space, potentially crashing
+/// when the limited amount is exhausted (stack overflow).
+///
+/// A Trampoline represents a computation which consists of steps.
+/// Each step is either more work which should be executed (`More`),
+/// in the form of a function which returns the next step,
+/// or a final value (`Done`), which indicates the end of the computation.
+///
+/// In trampolined programs, instead of each computation invoking
+/// the next computation, (i.e., calling functions, possibly recursing directly),
+/// they yield the next computation.
+///
+/// Trampolines can be executed through a control loop using the `run` method,
+/// and can be chained together using the `flatMap` method.
+///
 public class Trampoline<T> {
 
     internal init() {}
@@ -101,20 +101,20 @@ private final class FlatMap<A, B>: Trampoline<B> {
     internal override func resume() -> Either<() -> Trampoline<B>, B> {
         switch subroutine {
         case let done as Done<A>:
-            let continuation = self.continuation
-            return .left {
+            return .left { [continuation] in
                 continuation(done.result)
             }
         case let more as More<A>:
-            let continuation = self.continuation
-            return .left {
+            return .left { [continuation] in
                 more.next().flatMap(continuation)
             }
         default:
-            fatalError("""
+            fatalError(
+                """
                 FlatMap is not a valid subroutine.
                 Use flatMap to construct proper FlatMap structures.
-                """)
+                """
+            )
         }
     }
 }
